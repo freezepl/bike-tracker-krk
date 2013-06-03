b6dc857549ae14f22026d153a1c5ed29a5d85aa6
Łukasz Młodzik <lukasz.mlodzik@gmail.com>
2013-06-03 20:53:54 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BikeTrackerKrk::Application.config.secret_token = 'e0cec3ecb75ed6dda330c89983da604fd685d898223b28f11ab4d65fd82a275b116e56fba04fa8aac69f52b8aefd752d551a75cff80b5050c6a995097f536cac'
