a9af55f29a5633dc44cecaac766e7e8515647ee6
Lukasz Mlodzik <lukasz.mlodzik@ananastar.com>
2013-06-06 10:54:48 +0200

    Created home controller, changed ruby to 2.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 BikeTrackerKrk::Application.routes.draw do
 
   post '/route' => 'routes#save_route'
+  root :to => 'home#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
