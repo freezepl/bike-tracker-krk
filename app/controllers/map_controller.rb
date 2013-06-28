174c68bae4d6b8c8d0d4e0d2ef6a9831dfc50dbb
Przemysław Dąbek <przemek.dabek@getbase.com>
2013-06-28 19:04:19 +0200

    more underscore approach to handle coordinates

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,5 +2,6 @@ class MapController < ApplicationController
   def index
     @routes = Route.all
     @locations = @routes.map(&:locations).reduce(&:+)
+    @coordinates = @locations.map { |location| location[1..2] }
   end
 end
